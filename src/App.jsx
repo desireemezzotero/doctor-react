473bbbd2fad144e8ef7a25c6d5fcecb4e534fb1e
desireemezzotero <desireemezzotero1@gmail.com>
2025-02-24 17:36:08 +0100

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./pages/HomePage";
+import HeaderNav from "./components/HeaderNav";
+function App() {
+  return (
+    <>
+    <HeaderNav/>
+     <HomePage/>
+    </>
+  )
+}
+
+export default App
