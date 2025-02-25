23f254bc1e938044d6373ad854bbf4ad51865909
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-25 15:03:53 +0100

    feat: add default cards grid system in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,17 +5,29 @@ import CarouselHomePage from "../components/CarouselHomePage"
 import CardHomePage from "../components/CardHomePage"
 
 function HomePage() {
-  const {fechDataDoctors} = useGlobalContext()
+  const { fechDataDoctors } = useGlobalContext()
   console.log(fechDataDoctors)
 
   useEffect(fechDataDoctors, [])
 
   return (
     <>
-    <section className="h-[500px]">
-      <CarouselHomePage />
-    </section>
-    <CardHomePage/>
+      <section className="h-[500px]">
+        <CarouselHomePage />
+      </section>
+
+      <section className="container m-auto flex justify-center my-[50px]">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <CardHomePage />
+          <CardHomePage />
+          <CardHomePage />
+          <CardHomePage />
+          <CardHomePage />
+          <CardHomePage />
+          <CardHomePage />
+          <CardHomePage />
+        </div>
+      </section>
     </>
 
   )
