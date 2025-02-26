569e7ac1a8b90235e8c3b7df51876abc178649d7
desireemezzotero <desireemezzotero1@gmail.com>
2025-02-26 12:22:02 +0100

    color input search

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -38,13 +38,13 @@ function HomePage() {
 
             <input
               type="text"
-              id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+              id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-teal-500  focus:border-teal-500 "
               placeholder="Cerca un dottore..."
               name="search"
               onChange={handlerChange}
               required />
 
-            <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
+            <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-teal-500 hover:bg-teal-500 focus:ring-4 focus:outline-none focus:ring-teal-500 font-medium rounded-lg text-sm px-4 py-2">Search</button>
           </div>
         </form>
 
