daa2c8f77aed2ab9f7044d51da51a3932aef013a
desireemezzotero <desireemezzotero1@gmail.com>
2025-02-26 16:36:43 +0100

    add detailPage

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -7,66 +7,99 @@ function DetailPage() {
   const {doctorData,fechDataDoctor} = useGlobalContext()
   const {doctorName, doctorSurname,doctorTelephone,doctorMail,image_url, address,average_vote, specializations, reviews} = doctorData
 
-  useEffect(()=> fechDataDoctor(id),[])
+  useEffect(()=> {fechDataDoctor(id)},[id])
 
   return (
-  <div className="container mx-auto">
-    <div className="w-full h-[500px] bg-white border border-gray-200 rounded-lg shadow-sm">
-            <div className="h-[70%]">
-                <img className="rounded" src={image_url} alt={`Doctor ${doctorName} ${doctorSurname} image`} />
+  <div className="container mx-auto pt-[50px]">
+      <div className="doctor-card flex flex-wrap items-center justify-center gap-5">
+        <div className=" mr-[40px]">
+          <img className="rounded h-[350px] w-[300px]" src={image_url} alt={`Doctor ${doctorName} ${doctorSurname} image`} />
+        </div>
+
+
+        <div className="p-4 align-middle">
+          <h3 className="text-3xl font-semibold tracking-tight text-gray-900">{`${doctorName} ${doctorSurname}`}</h3>
+            <div className="description-doctor text-lg pt-5">
+
+             <p className="mb-3">
+               <i className="fa-solid fa-phone"></i>
+               <span className="text-gray-900 pl-3">{doctorTelephone}</span>
+              </p>
+
+              <p className="mb-3">
+                <i className="fa-solid fa-envelope"></i>
+                <span className="text-gray-900 pl-3">{doctorMail}</span>
+              </p>
+
+              <p className="mb-3">
+                <i className="fa-solid fa-location-dot"></i>
+                <span className="text-gray-900 pl-3">{address}</span>
+              </p>
             </div>
-            <div className="h-[30%] flex flex-col items-between justify-center">
-                <div className="p-4">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900">{`${doctorName} ${doctorSurname}`}</h5>
-                    {specializations?.map(speciality => {
-                      <div>
-                      <img>{speciality?.specialityIcon}</img>
-                      <span className="text-gray-900">{speciality?.specialityName}</span>
-                      <span className="text-gray-900">{speciality?.specialityDescription}</span>
+        </div>
+      
+       <div className="flex items-center"> 
+          <div className="p-4 border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 ">
+            <h3 className="text-xl font-semibold tracking-tight text-gray-900 mb-3">Specializzazioni:</h3>
+           {specializations?.map(speciality => {
+            return (
+            <div key = {speciality?.specialityName} className="flex">
+              <img src={speciality?.specialityIcon} className= "w-[50px] h-[50px] mb-3"></img>
+              <p>
+                <span className="text-gray-900 ml-7 font-bold">{speciality?.specialityName}: </span>
+                <span className="text-gray-900s ml-2">{speciality?.specialityDescription}</span>
+              </p>
+            </div>
+           )})}
+          </div>
+        </div>
+
+      </div>
+
+
+    <div className="flex items-center justify-center mt-[50px]"> 
+      <div className="p-4 border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 ">
+        <h3 className="text-xl font-semibold tracking-tight text-gray-900 mb-3">Recensioni:</h3>
+
+          <div className="flow-root">
+            <ul role="list" className="divide-y divide-gray-200">
+
+              {reviews?.map(review => {
+                return (
+                  <li className="py-3 sm:py-4" key= {review.id}>
+                    <div className="flex items-center">
+
+                      <div className="shrink-0">
+                        <i class="fa-solid fa-user w-8 h-8"></i>
                       </div>
-                    })}
-                    <span className="text-gray-900">{doctorTelephone}</span>
-                    <span className="text-gray-900">{doctorTelephone}</span>
-                    <span className="text-gray-900">{doctorMail}</span>
-                    <span className="text-gray-900">{address}</span>
-                    <div className="mt-4 flex justify-between items-center">
-                        <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                            </svg>
-                            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                            </svg>
-                            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                            </svg>
-                            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                            </svg>
-                            <svg className="w-4 h-4 text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                            </svg>
-                            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm ms-3">{average_vote}</span>
-                        </div>
-                        {reviews?.map(review=> {
+
+                      <div>
+                        <p className="text-sm"> {review.date}</p>
+                        <p className="font-bold">{review.name}</p>
+                        <p className="font-bold flex">{review.title}: {review.vote} </p>
+                        <p>{review.description}</p>
+                      </div>
+                    </div>
+                  </li>
+                )}
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+      </div>
+    
+/*               
+       {reviews?.map(review=> {
                             <div>
-                              <span className="text-gray-900">{review?.title}</span>
-                              <span className="text-gray-900">{review?.description}</span>
+                              <span className="text-gray-900"></span>
+                              <span className="text-gray-900"></span>
                               <span className="text-gray-900">{review?.vote}</span>
                               <span className="text-gray-900">{review?.date}</span>
                             </div>
-                         })}
-                    </div>
-                </div>
-            </div>
-        </div >
-
-        <div>
-          <h1>{doctorName}</h1>
-
-        </div>
-    </div>
+                         })} */
   )
 }
 
 export default DetailPage
+
