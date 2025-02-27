f729c51c689cb2b03ac4301161c79bb91f32cc52
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-27 10:27:22 +0100

    fix: texts in the pages

--- src/components/CarouselHomePage.jsx
+++ src/components/CarouselHomePage.jsx
@@ -16,7 +16,7 @@ function CarouselHomePage() {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 3000,
           disableOnInteraction: false,
         }}
         pagination={{
