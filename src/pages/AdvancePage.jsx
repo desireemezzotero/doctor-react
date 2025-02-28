3c94ef5af4e0f347dc249b0a3390ab8e6fa239b1
desireemezzotero <desireemezzotero1@gmail.com>
2025-02-28 12:14:12 +0100

    filteredSpecialities

--- src/pages/AdvancePage.jsx
+++ src/pages/AdvancePage.jsx
@@ -1,19 +1,21 @@
 import { useGlobalContext } from "../context/GlobalContext"
-import { useEffect, useState } from "react"
 
-import CarouselHomePage from "../components/CarouselHomePage"
+import { useEffect, useState } from "react";
+
 import CardHomePage from "../components/CardHomePage"
+import FilteredSpecialities from "../components/FilteredSpecialities";
+
 
 function AdvancePage() {
-  const { fechDataDoctors, doctorsData } = useGlobalContext()
+  const { fechDataDoctors, doctorsData} = useGlobalContext()
   const [searchTerm, setSearchTerm] = useState('')
 
   const handlerChange = (e) => {
     setSearchTerm(e.target.value)
   }
-
+ 
   const filteredDoctors = doctorsData.filter(doc => {
-    if (doc.name.toLowerCase().includes(searchTerm.toLowerCase()) || doc.surname.toLowerCase().includes(searchTerm.toLowerCase()) || doc.name_speciality.toLowerCase().includes(searchTerm.toLowerCase())) {
+    if (doc.name.toLowerCase().includes(searchTerm.toLowerCase()) || doc.surname.toLowerCase().includes(searchTerm.toLowerCase())) {
       return doc
     }
   })
@@ -23,9 +25,11 @@ function AdvancePage() {
   return (
     <>
       <section className="container mt-[130px] m-auto">
-        <form className="max-w-md mx-auto">
-          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
-          <div className="relative">
+
+        <div className="flex justify-around align-middle">
+         <form>
+           <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+           <div className="relative">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
@@ -39,11 +43,13 @@ function AdvancePage() {
               name="search"
               onChange={handlerChange}
               required />
+           </div>
+          </form>
 
-            <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-teal-500 hover:bg-teal-500 focus:ring-1 focus:outline-none font-medium rounded-lg text-sm px-4 py-2">Search</button>
-          </div>
-        </form>
-
+         <FilteredSpecialities/>
+          
+        </div> 
+        
           {filteredDoctors.length === 0 ? 
                 <div className="border bg-gray-200 text-center mb-5 mt-5">
                   <h3 className="text-xl font-semibold tracking-tight text-gray-900 mb-3 mt-4 text-center">
@@ -62,6 +68,10 @@ function AdvancePage() {
           </div>
         </div>
       </section>
+
+
+
+
     </>
 
   )
